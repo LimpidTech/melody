23463a2036643f08aecbcd3552dbce9cd77d4b0c
Bailey Stoner <monokrome@monokro.me>
2017-12-25 23:56:55 -0800

    Add setup.cfg

--- melody/settings/development.py
+++ melody/settings/development.py
@@ -1,5 +1,22 @@
+import os
+
 from melody.settings.defaults import *
 
 DEBUG = True
-ROOT_URLCONF = 'melody.core.urls'
+
+ROOT_URLCONF = 'melody.core.urls.development'
 SECRET_KEY = env_value('secret_key', 'diagonal stunning powder ledge employ dealer')
+
+STATIC_URL = '/static/'
+STATIC_ROOT = project_path('static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = project_path('media')
+
+INSTALLED_APPS += [
+    'django.contrib.staticfiles',
+]
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
