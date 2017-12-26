d27eea1ab53c2fd9313951e616786b76c1c3639d
Bailey Stoner <monokrome@monokro.me>
2017-12-26 02:19:29 -0800

    Extend data model to include categorization.

--- melody/posts/models.py
+++ melody/posts/models.py
@@ -4,3 +4,21 @@ from melody.core import models
 class Post(models.UUIDModel):
     subject = models.TextField()
     body = models.TextField()
+
+
+class Category(models.UUIDModel):
+    name = models.TextField()
+
+    posts = models.ManyToManyField(
+        Post,
+        related_name='cateogores',
+        null=True,
+        blank=True,
+    )
+
+    parent = models.ForeignKey(
+        'self',
+        on_delete=models.DO_NOTHING,
+        null=True,
+        blank=True,
+    )
