09ce86944c812786b1c4c44fae3b1cdc3b12de5d
Bailey Stoner <monokrome@monokro.me>
2018-01-09 22:58:46 -0800

    Add dj_database_url for Heroku deployments

--- melody/settings/production.py
+++ melody/settings/production.py
@@ -1,5 +1,11 @@
+import dj_database_url
+
 from melody.settings.defaults import *
 
 DEBUG = True
 
 ALLOWED_HOSTS = ['services.melody.monokro.me']
+
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=500),
+}
