437dfc81d1c7d76c70a677390622169dbb14f706
Bailey Stoner <monokrome@monokro.me>
2018-01-09 23:50:03 -0800

    Add FRONTEND_URL

--- melody/settings/production.py
+++ melody/settings/production.py
@@ -5,6 +5,7 @@ from melody.settings.defaults import *
 DEBUG = True
 
 ALLOWED_HOSTS = ['services.melody.monokro.me']
+FRONTEND_URL = 'melody.monokro.me'
 ROOT_URLCONF = 'melody.core.urls.production'
 SECRET_KEY = env_value('secret_key')
 
