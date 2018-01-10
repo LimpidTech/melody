1a568479966df5f1758d797b560a18b58d71276b
Bailey Stoner <monokrome@monokro.me>
2018-01-09 23:54:30 -0800

    Use env_value so we don't have to commit to fix this

--- melody/settings/production.py
+++ melody/settings/production.py
@@ -8,8 +8,7 @@ ALLOWED_HOSTS = ['services.melody.monokro.me']
 FRONTEND_URL = 'melody.monokro.me'
 ROOT_URLCONF = 'melody.core.urls.production'
 SECRET_KEY = env_value('secret_key')
-STATIC_ROOT = project_path('static')
-STATIC_URL = '/static/'
+STATIC_URL = env_value('static_url')
 
 DATABASES = {
     'default': dj_database_url.config(conn_max_age=500),
