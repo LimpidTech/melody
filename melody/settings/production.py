4cc377b15171732b8c7791148c456b6bc5c55b26
Bailey Stoner <monokrome@monokro.me>
2018-01-09 23:04:01 -0800

    Add SECRET_KEY

--- melody/settings/production.py
+++ melody/settings/production.py
@@ -5,6 +5,7 @@ from melody.settings.defaults import *
 DEBUG = True
 
 ALLOWED_HOSTS = ['services.melody.monokro.me']
+SECRET_KEY = env_value('secret_key')
 
 DATABASES = {
     'default': dj_database_url.config(conn_max_age=500),
