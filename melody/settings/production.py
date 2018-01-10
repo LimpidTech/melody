8e6a993ff66f6c937b47dac66d3100443a0152ad
Bailey Stoner <monokrome@monokro.me>
2018-01-09 23:53:00 -0800

    Add fake STATIC_URL / STATIC_ROOT

--- melody/settings/production.py
+++ melody/settings/production.py
@@ -8,6 +8,8 @@ ALLOWED_HOSTS = ['services.melody.monokro.me']
 FRONTEND_URL = 'melody.monokro.me'
 ROOT_URLCONF = 'melody.core.urls.production'
 SECRET_KEY = env_value('secret_key')
+STATIC_ROOT = project_path('static')
+STATIC_URL = '/static/'
 
 DATABASES = {
     'default': dj_database_url.config(conn_max_age=500),
