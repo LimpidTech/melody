54593eb9d245310ba2353974a83f8e26e02c7379
Bailey Stoner <monokrome@monokro.me>
2018-01-12 00:02:13 -0800

    Add fields to MailSerializer

--- melody/mail/api/serializers.py
+++ melody/mail/api/serializers.py
@@ -1,4 +1,15 @@
 from rest_framework import serializers
 
 class MailSerializer(serializers.Serializer):
-    pass
+    event = serializers.CharField()
+    timestamp = serializers.IntegerField()
+
+    domain = serializers.CharField()
+    code = serializers.IntegerField()
+
+    recipient = serializers.CharField()
+    message_id = serializers.CharField(source='Message-id')
+    attachment_count = serializers.IntegerField(source='attachment-count')
+
+    body_plain = serializers.CharField(source='body-plain')
+    message_headers = serializers.JSONField(source='message-headers')
