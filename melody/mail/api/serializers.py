300ce9a9e60393ab5b3b3d8cb9af3be69f682207
Bailey Stoner <monokrome@monokro.me>
2018-01-12 00:19:38 -0800

    Set up Open fields

--- melody/mail/api/serializers.py
+++ melody/mail/api/serializers.py
@@ -1,15 +1,27 @@
 from rest_framework import serializers
 
 class MailSerializer(serializers.Serializer):
-    event = serializers.CharField()
+    type = serializers.CharField(source='event')
     timestamp = serializers.IntegerField()
 
     domain = serializers.CharField()
-    code = serializers.IntegerField()
 
     recipient = serializers.CharField()
     message_id = serializers.CharField(source='Message-id')
     attachment_count = serializers.IntegerField(source='attachment-count')
 
-    body_plain = serializers.CharField(source='body-plain')
-    message_headers = serializers.JSONField(source='message-headers')
+    plain_text = serializers.CharField(source='body-plain')
+
+    city = serializers.CharField()
+    country = serializers.CharField()
+    region = serializers.CharField()
+
+    client_name = serializers.CharField(source='client-name')
+    client_os = serializers.CharField(source='client-os')
+    client_type = serializers.CharField(source='client-type')
+
+    user_agent = serializers.CharField(source='user_agent')
+    ip_address = serializers.CharField(source='ip')
+
+    # message_headers = serializers.JSONField(source='message-headers')
+    # code = serializers.IntegerField()
