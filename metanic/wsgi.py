f9abfbc661d5446b710eba622774642554459304
Bailey Stoner <monokrome@monokro.me>
2018-03-22 01:06:52 -0700

    Maybe I don't need this?

--- metanic/wsgi.py
+++ metanic/wsgi.py
@@ -1,10 +1,7 @@
 import os
 
-from raven.contrib.django.raven_compat.models import client
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "metanic.settings.production")
 
-client.captureException()
 application = get_wsgi_application()
