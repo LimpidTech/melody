6bdc5d026be3df559383fe3b2a390d08b429fe13
Bailey Stoner <monokrome@monokro.me>
2018-05-13 08:41:59 -0700

    Move password_verification to the proper serialzier.

--- metanic/accounts/api/serializers.py
+++ metanic/accounts/api/serializers.py
@@ -4,6 +4,21 @@ from django.contrib.auth import models as auth_models
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+    email = serializers.EmailField()
+
+    password = serializers.CharField(write_only=True)
+    password_verification = serializers.CharField(write_only=True)
+
+    username = serializers.CharField()
+
+    def validate(self, data):
+        if data['password'] != data['password_verification']:
+            return serializers.ValidationError(
+                "The verification password must be the same as the password."
+            )
+
+        return super(AuthenticationSerializer, self).is_valid()
+
     class Meta(object):
         model = auth_models.User
 
@@ -16,14 +31,4 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 
 class AuthenticationSerializer(serializers.Serializer):
     username = serializers.CharField()
-
     password = serializers.CharField(write_only=True)
-    password_verification = serializers.CharField(write_only=True)
-
-    def validate(self, data):
-        if data['password'] != data['password_verification']:
-            return serializers.ValidationError(
-                "The verification password must be the same as the password."
-            )
-
-        return super(AuthenticationSerializer, self).is_valid()
