38712e77f61cc00e16b8afa5e803574c64299d44
Bailey Stoner <monokrome@monokro.me>
2018-05-18 19:52:07 -0700

    Improve CORS implementation.

--- metanic/core/middleware.py
+++ metanic/core/middleware.py
@@ -4,23 +4,27 @@ from django.conf import settings
 from django import http
 from django import urls
 
-CORS_ALLOWED_ORIGINS = getattr(settings, 'CORS_ALLOWED_ORIGINS', [])
+ACCESS_CONTROL_MAX_AGE = getattr(settings, 'ACCESS_CONTROL_MAX_AGE', 0)
 
-CORS_ALLOWED_HEADERS = getattr(
-    settings, 'CORS_ALLOWED_HEADERS', [
+ACCESS_CONTROL_ALLOW_ORIGINS = set(getattr(settings, 'ACCESS_CONTROL_ALLOW_ORIGINS', []))
+
+ACCESS_CONTROL_ALLOW_HEADERS = set(map(str.lower, getattr(
+    settings, 'ACCESS_CONTROL_ALLOW_HEADERS', [
         'Content-Type',
         'Accept',
     ]
-)
+)))
 
-CORS_ALLOWED_METHODS = getattr(
-    settings, 'CORS_ALLOWED_METHODS',
+ACCESS_CONTROL_ALLOW_METHODS = set(map(str.upper, getattr(
+    settings, 'ACCESS_CONTROL_ALLOW_METHODS',
     ['POST', 'GET', 'OPTIONS', 'PUT', 'DELETE']
-)
+)))
 
 HTTP_ORIGIN = 'HTTP_ORIGIN'
 HTTP_ACR_METHOD = 'HTTP_ACCESS_CONTROL_REQUEST_METHOD'
 
+# This is here so that we only process the value once instead of for every request
+ACCESS_CONTROL_ALLOW_METHODS_VALUE = ','.join(ACCESS_CONTROL_ALLOW_METHODS)
 
 def match(pattern, origin):
     origin_length = len(origin)
@@ -46,6 +50,9 @@ def match(pattern, origin):
 
 
 def origin_is_match(patterns, origin):
+    if origin is None:
+        return False
+
     for pattern in patterns:
         if match(pattern, origin):
             return True
@@ -53,34 +60,67 @@ def origin_is_match(patterns, origin):
 
 
 class CORSMiddleware(object):
-    """ Provides necessary responses and headers for CORS requests. """
+    """ Provides necessary responses and headers for CORS requests.
 
-    # TODO:
-    #
-    #  - Verify that this matches the spec (it probably doesn't yet :D)
-    #
-    #  - Consider only using this for dev. There's no reason for these requests
-    #  to hit Django at all in production unless we start varying access by API
-    #  resource permissions or similar.
+    The process that this implements is defined in the Cross-Origin Resource
+    Sharing specification - which can be found here:
+
+    https://www.w3.org/TR/cors/#resource-requests
+
+    """
 
     def __init__(self, get_response):
         self.get_response = get_response
 
+    def request_supports_credentials(self, request):
+        """ Returns True if the given request supports credentials. Otherwise, False.
+
+        TODO: This should actually attempt to detect credentials support
+
+        """
+
+        return True
+
     def __call__(self, request):
         response = self.get_response(request)
-
-        method = request.META.get(HTTP_ACR_METHOD, None)
-
-        if request.method == 'OPTIONS' and not method:
-            return http.HttpResponseForbidden()
-
         origin = request.META.get(HTTP_ORIGIN, None)
 
-        if origin and origin_is_match(CORS_ALLOWED_ORIGINS, origin):
-            response['Access-Control-Allow-Origin'] = request.META['HTTP_ORIGIN']
-            response['Access-Control-Allow-Methods'] = ",".join(CORS_ALLOWED_METHODS)
-            response['Access-Control-Allow-Headers'] = ",".join(CORS_ALLOWED_HEADERS)
+        if not origin_is_match(ACCESS_CONTROL_ALLOW_ORIGINS, origin):
+            # We have an Origin header, but it doesn't match ALLOW origins. Don't allow CORS here.
+            return response
+
+        response['Access-Control-Allow-Origin'] = origin
+
+        if self.request_supports_credentials(request):
+            # TODO: We should verify that the value of origin is not ` * `` here
             response['Access-Control-Allow-Credentials'] = 'true'
+
+        if request.method != 'OPTIONS':
+            response['Access-Control-Allow-Headers'] = ACCESS_CONTROL_ALLOW_HEADERS
+            return response
+
+        # At this point, we know that we have a pre-flight request
+        requested_method = request.META.get(HTTP_ACR_METHOD, None)
+        requested_headers = request.META.get('Access-Control-Request-Headers', []).split(',')
+
+        for requested_method not in ALLOW_METHODS:
+            return response
+
+        # The spec requires these to be ASCII and case-insensitive, so lower() is a safe comparison
+        # in this case. Note that we intentionally don't do this above in some cases in order to
+        # avoid any potential hacks using UTF-8 characters.
+        ALLOW_and_requested_header_values = []
+
+        for header in requested_headers:
+            if header.lower() not in ALLOW_HEADERS:
+                # Since the header isn't ALLOW, we don't allow the requested request
+                return response
+
+            ALLOW_and_requested_header_values.append(header)
+
+        response['Access-Control-Max-Age'] = ACCESS_CONTROL_MAX_AGE
+        response['Access-Control-Allow-Methods'] = ACCESS_CONTROL_ALLOW_METHODS_VALUE
+        response['Access-Control-Allow-Headers'] = ','.join(ALLOW_and_requested_header_values)
 
         return response
 
