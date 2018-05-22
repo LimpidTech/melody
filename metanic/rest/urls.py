83cf4ca7a9ba210ccb3359a89c4e0253479e4e7c
Bailey Stoner <monokrome@monokro.me>
2018-05-22 01:35:11 -0700

    Add an endpoint for creating JWT tokens

--- metanic/rest/urls.py
+++ metanic/rest/urls.py
@@ -1,7 +1,11 @@
 from django import urls
 
+from rest_framework_jwt import views
+
 from . import routing
 
 routing.router.autodiscover()
 
-urlpatterns = routing.router.urls
+urlpatterns = routing.router.urls + [
+    urls.path('jwt/', views.obtain_jwt_token),
+]
