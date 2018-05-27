8859bf83ca796981da07aeb10fae7024673098fb
Bailey Stoner <monokrome@monokro.me>
2018-05-26 23:36:03 -0700

    Remove unnecessary print.

--- metanic/wsgi.py
+++ metanic/wsgi.py
@@ -4,6 +4,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'metanic.settings.production'
 
-print('ok ' * 100)
-
 application = get_wsgi_application()
