19b0976183b5c7f3a08ddb2257e24955ef8edb9e
Bailey Stoner <monokrome@monokro.me>
2018-06-21 22:58:37 -0700

    Repair issue where serializer wasn't working for new Collection format.

--- metanic/collector/api/serializers.py
+++ metanic/collector/api/serializers.py
@@ -11,33 +11,38 @@ def get_serializer(module_name, attribute_name):
     return getattr(importlib.import_module(module_name), attribute_name)
 
 
-class MultiResourceRelatedField(serializers.RelatedField):
-    def to_representation(self, obj):
-        serializer_data = COLLECTION_SERIALIZER_TYPES.get(obj._meta.model_name)
+def get_serializer_data(context, collection_item):
+    serializer_data = COLLECTION_SERIALIZER_TYPES.get(
+        collection_item._meta.model_name
+    )
 
-        if serializer_data is None:
-            raise ValueError('{} must be in COLLECTION_SERIALIZER_TYPES'.format(
-                obj._meta.model_name,
-            ))
+    if serializer_data is None:
+        raise ValueError('{} must be in COLLECTION_SERIALIZER_TYPES'.format(
+            collection_item._meta.model_name,
+        ))
 
-        Serializer = get_serializer(*serializer_data)
+    Serializer = get_serializer(*serializer_data)
 
-        if Serializer is None:
-            raise ValueError('No serializer exists at {}.{}'.format(
-                *serializer_data,
-            ))
+    if Serializer is None:
+        raise ValueError('No serializer exists at {}.{}'.format(
+            *serializer_data,
+        ))
 
-        return Serializer(
-            obj,
-            context=self.context,
-        ).data
+    return Serializer(collection_item, context=context).data
 
 
 class CollectionSerializer(serializers.Serializer):
     url = serializers.HyperlinkedIdentityField(view_name='collection-detail')
     name = serializers.SerializerMethodField()
 
-    items = MultiResourceRelatedField(many=True, read_only=True)
+    items = serializers.SerializerMethodField()
+
+    def get_items(self, collection):
+        return list(map(
+            get_serializer_data,
+            [self.context],
+            collection.items(self.context['request']),
+        ))
 
     def get_name(self, collection_item):
         collection_name_field = getattr(
