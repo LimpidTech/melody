a75b0d20cc138ee2e86bc09691d2090d63f5f403
Bailey Stoner <monokrome@monokro.me>
2018-08-12 23:42:36 -0700

    Fix module not being callable.

--- metanic/__main__.py
+++ metanic/__main__.py
@@ -4,4 +4,12 @@ import sys
 
 from django.core import management
 
-management.execute_from_command_line(sys.argv)
+
+def execute_command_line():
+    management.execute_from_command_line(sys.argv)
+
+
+# We need this check to ensure we didn't import this from metanic, but that
+# we are executing it as metanic's main entry.
+if __name__ == '__main__':
+    execute_from_command_line()
