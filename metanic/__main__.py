254b5123bb2ac80ec627e2b0badaa3e346cec628
Bailey Stoner <monokrome@monokro.me>
2018-08-12 23:45:53 -0700

    Byebye weirdness

--- metanic/__main__.py
+++ metanic/__main__.py
@@ -1,15 +1,5 @@
 #!/usr/bin/env python
 
-import sys
+from metanic import command_line
 
-from django.core import management
-
-
-def execute_command_line():
-    management.execute_from_command_line(sys.argv)
-
-
-# We need this check to ensure we didn't import this from metanic, but that
-# we are executing it as metanic's main entry.
-if __name__ == '__main__':
-    execute_from_command_line()
+command_line.execute()
